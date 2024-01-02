fc3ac5445b77954b365e6e0d3a887f4ec1b89c0b
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-02 20:44:05 -0300

    configurações inicias feitas

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1 class="text-red-400 font-bold text-4xl">Home</h1>
+        <h2>Home</h2>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Home',
+}
+</script>
+
+<style scoped>
+</style>
