ef0c1b45f41121403339f7bc629aa934b1c5231e
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-03 20:09:25 -0300

    renderizando todas as informações vinda da API

--- src/views/Series.vue
+++ src/views/Series.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
-        <h1>Series</h1>
         <router-link to="/">Redirect to home</router-link>
     </div>
-    <div v-for="serie in series" :key="serie.id">
+    <div v-for="serie in series" :key="serie.id" class="flex flex-col p-10 border border-black m-5">
+        <img :src="serie.capa" alt="Capa da Serie" class="w-auto h-60">
         <h1>{{ serie.titulo }}</h1>
+        <p>{{ serie.sinopse }}</p>
+        <p>{{ serie.genero }}</p>
+        <p>Ano: {{ serie.ano }}</p>
+        <p>Temporada: {{ serie.temporadas }}</p>
+        <p>Total de Episódios: {{ serie.episodios }}</p>
+        <p>Classificação: {{ serie.classificacao }}</p>
     </div>
 </template>
 
