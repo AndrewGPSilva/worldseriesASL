d25a08511c0b798046e9fd745b232b7d0cea26cb
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-03 20:00:59 -0300

    recebendo os dados da API, interface dos dados que chegam com typescript

--- src/views/Series.vue
+++ src/views/Series.vue
@@ -3,11 +3,42 @@
         <h1>Series</h1>
         <router-link to="/">Redirect to home</router-link>
     </div>
+    <div v-for="serie in series" :key="serie.id">
+        <h1>{{ serie.titulo }}</h1>
+    </div>
 </template>
 
 <script lang="ts">
+
+interface ISerie {
+    id: number;
+    titulo: string;
+    capa: string;
+    genero: string;
+    sinopse: string;
+    ano: number;
+    temporadas: number;
+    episodios: number;
+    classificacao: number;
+}
+
+import axios from 'axios'
 export default {
-    name: 'Series'
+    name: 'Series',
+    data() {
+        return {
+            series: [] as ISerie[]
+        }
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/series')
+            .then(({ data }) => {
+                this.series = data
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 }
 </script>
 
