a029517fb6802db176c3402f955cd390a5e45e87
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-04 11:13:49 -0300

    iniciando card de exibição das séries

---
+++ src/components/Series/Allseries.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-for="serie in series" :key="serie.id"
+        class="group flex flex-col justify-start mx-6 items-start gap-2 w-96 h-56 duration-500 relative rounded-lg p-4 bg-gray-900 hover:-translate-y-2 hover:shadow-xl shadow-gray-800">
+        <div alt="image here"
+            class="absolute duration-700 shadow-md group-hover:-translate-y-4 group-hover:-translate-x-4 -bottom-10 -right-10 w-1/2 h-1/2 rounded-lg bg-gray-800">
+            <img :src="serie.capa" class="w-full h-full rounded-lg cursor-pointer" />
+        </div>
+
+        <div class="">
+            <h2 class="text-2xl font-bold mb-2 text-gray-100">{{ serie.titulo }}</h2>
+            <p class="text-gray-200 line-clamp-3">
+                {{ serie.sinopse  }}
+            </p>
+        </div>
+        <router-link to="/"><button class="hover:bg-gray-700 bg-gray-800 text-gray-100 mt-6 rounded p-2 px-6">
+            Explorar
+        </button></router-link>
+    </div>
+</template>
+
+<script lang="ts">
+
+interface ISerie {
+    id: number;
+    titulo: string;
+    capa: string;
+    genero: string;
+    sinopse: string;
+    ano: number;
+    temporadas: number;
+    episodios: number;
+    classificacao: number;
+}
+
+import axios from 'axios'
+export default {
+    name: 'Allseries',
+    data() {
+        return {
+            series: [] as ISerie[]
+        }
+    },
+    mounted() {
+        axios.get('http://127.0.0.1:8000/api/series')
+            .then(({ data }) => {
+                this.series = data
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+}
+</script>
--- src/views/Series.vue
+++ src/views/Series.vue
@@ -1,49 +1,16 @@
 <template>
-    <div>
-        <router-link to="/">Redirect to home</router-link>
+    <div class="flex p-5">
+        <Allseries />
     </div>
-    <div v-for="serie in series" :key="serie.id" class="flex flex-col p-10 border border-black m-5">
-        <img :src="serie.capa" alt="Capa da Serie" class="w-auto h-60">
-        <h1>{{ serie.titulo }}</h1>
-        <p>{{ serie.sinopse }}</p>
-        <p>{{ serie.genero }}</p>
-        <p>Ano: {{ serie.ano }}</p>
-        <p>Temporada: {{ serie.temporadas }}</p>
-        <p>Total de Episódios: {{ serie.episodios }}</p>
-        <p>Classificação: {{ serie.classificacao }}</p>
-    </div>
+    <router-link to="/">Redirect to home</router-link>
 </template>
 
 <script lang="ts">
-
-interface ISerie {
-    id: number;
-    titulo: string;
-    capa: string;
-    genero: string;
-    sinopse: string;
-    ano: number;
-    temporadas: number;
-    episodios: number;
-    classificacao: number;
-}
-
-import axios from 'axios'
+import Allseries from '../components/Series/Allseries.vue'
 export default {
     name: 'Series',
-    data() {
-        return {
-            series: [] as ISerie[]
-        }
-    },
-    mounted() {
-        axios.get('http://127.0.0.1:8000/api/series')
-            .then(({ data }) => {
-                this.series = data
-            })
-            .catch(err => {
-                console.log(err)
-            })
+    components: {
+        'Allseries': Allseries
     }
 }
 </script>
