257144ac61e382dfbab6a8d71162480878745b40
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-22 20:31:48 -0300

    componente de carrossel de imagens

---
+++ src/components/Guide/Carousel/Carousel.vue
@@ -0,0 +1,59 @@
+<template lang="">
+    <div class="container">
+        <button @click="nextImage">
+            <i class="pi pi-arrow-left" style="color: white"></i>
+        </button>
+        <img :src="currentImage" alt="">
+        <button @click="previousImage">
+            <i class="pi pi-arrow-right" style="color: white"></i>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Carousel",
+    data() {
+        return {
+            images: [
+                "https://i.pinimg.com/originals/d1/00/92/d1009254c0370706696faad1800154aa.jpg",
+                "https://img.elo7.com.br/product/zoom/1BDDCE2/painel-festa-1x0-70cm-one-piece-personalizado.jpg",
+            ],
+            currentIndex: 0
+        };
+    },
+    computed: {
+        currentImage() {
+            return this.images[this.currentIndex];
+        }
+    },
+    methods: {
+        nextImage() {
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        },
+        previousImage() {
+            this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+        }
+    }
+};
+</script>
+
+<style scoped>
+button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+}
+.container {
+    display: flex;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #141414;
+}
+.container img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    border-radius: 20px;
+}
+</style>
--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -4,17 +4,20 @@
     </header>
     <main>
         <Message />
+        <Carousel />
     </main>
 </template>
 
 <script lang="ts">
 import Navbar from '../components/Guide/Navbar/Navbar.vue'
 import Message from '../components/Guide/Message/Message.vue'
+import Carousel from '../components/Guide/Carousel/Carousel.vue'
 export default {
     name: 'Guide',
     components: {
         'Navbar': Navbar,
-        'Message': Message
+        'Message': Message,
+        'Carousel': Carousel
     }
 }
 </script>
